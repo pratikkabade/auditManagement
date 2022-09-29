770fead42a337c4aeab2b752138b263778057a1d
Prãt!k Kabade <pratikkabade@hotmail.com>
2022-09-29 19:13:28 +0530

    Database changes

--- backend/Entities/DataBaseContext.cs
+++ backend/Entities/DataBaseContext.cs
@@ -9,14 +9,13 @@ namespace BackendAPI.Models
         {
         }
         public DbSet<Users> User { get; set; }
-        public DbSet<ContactUs> ContactMsg { get; set; }
-        public DbSet<Order> Orders { get; set; }
+        public DbSet<Form> ContactMsg { get; set; }
+
 
         // GIVING PREDIFINED DATA TO DATABASE 
-        // CREATING ADMIN USER
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // CREATING SAMPLE USER
+            // CREATING ADMIN USER
             modelBuilder.Entity<Users>().HasData(
                 new Users
                 {
@@ -27,27 +26,22 @@ namespace BackendAPI.Models
                 }
                 );
 
-            // ADDING DEFAULT CONTACTUS DATA
-            modelBuilder.Entity<ContactUs>().HasData(
-                new ContactUs
+            // CREATING Form
+            modelBuilder.Entity<Form>().HasData(
+                new Form
                 {
                     Id = 1,
-                    Date = DateTime.Now,
-                    Name = "sample",
-                    Email = "sample@localhost",
-                    Message = "Hello, I am sample user. I am facing some issues with the website. Please help me out."
-                });
-
-            // ADDING DEFAULT ORDER DATA
-            modelBuilder.Entity<Order>().HasData(
-                new Order
-                {
-                    TransactionId = 1,
-                    MemberId = 1,
-                    ProductName = "Product",
-                    RequiredDate = DateTime.Now,
-                    Quantities = 100,
-                    UserId = 1
+                    question0 = "sample",
+                    question1 = "sample",
+                    question2 = "sample",
+                    question3 = "sample",
+                    question4 = "sample",
+                    question5 = "sample",
+                    question6 = "sample",
+                    question7 = "sample",
+                    question8 = "sample",
+                    question9 = "sample",
+                    Email = "admin@localhost"
                 });
         }
     }
